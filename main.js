8b95788e6b7c90cb5d68f2c39925a014080c2ae1
agoode-sys <agoode@conncoll.edu>
2025-12-15 23:58:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-d3.json("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson").then((geojson) => {
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then((geojson) => {
     d3.dsv(",", "Global_Music_Streaming_Listener_Preferences.csv", d => {
         return {
             country: d.Country,
@@ -9,19 +9,30 @@ d3.json("https://raw.githubusercontent.com/datasets/geo-countries/master/data/co
         console.log("loaded csv", data);
         console.log("loaded json", geojson);
 
+        // matching csv names to geojson names
+        const countryMapping = {
+            "United States": "USA",
+            "UK": "United Kingdom",
+            "South Korea": "Korea",
+        };
+
+        function fixCountryName(csvCountry) {
+            return countryMapping[csvCountry] || csvCountry;
+        }
+
         // join the data by country
         let datajoin = {};
         data.forEach(d => {
-            if (!datajoin[d.country]) {
-                datajoin[d.country] = {
+            const fixedCountry = fixCountryName(d.country);
+            if (!datajoin[fixedCountry]) {
+                datajoin[fixedCountry] = {
                     platforms: {},
                     genres: {}
                 };
             }
-            datajoin[d.country].platforms[d.platform] = (datajoin[d.country].platforms[d.platform] || 0) + 1;
-            datajoin[d.country].genres[d.genre] = (datajoin[d.country].genres[d.genre] || 0) + 1;
+            datajoin[fixedCountry].platforms[d.platform] = (datajoin[fixedCountry].platforms[d.platform] || 0) + 1;
+            datajoin[fixedCountry].genres[d.genre] = (datajoin[fixedCountry].genres[d.genre] || 0) + 1;
         });
-        console.log(datajoin);
 
         // calculate top platform
         Object.keys(datajoin).forEach(country => {
@@ -37,81 +48,47 @@ d3.json("https://raw.githubusercontent.com/datasets/geo-countries/master/data/co
                 .map(([genre]) => genre);
         });
 
-        function getColor(d) {
-            if(d=="Spotify") {
-                return "#19e84d";
-            } else if (d=="YouTube") {
-                return '#e8191c';
-            } else if (d=="Amazon Music") {
-                return '#3519e8';
-            } else if (d=="Apple Music") {
-                return '#f77bea';
-            } else if (d=="Deezer") {
-                return '#8112d1';
-            } else if (d=="Tidal") {
-                return '#d9e812';
-            } else {
-                return '#555';
-            }
-        }
-
         geojson.features.forEach(feature => {
-            const countryname = feature.properties.ADMIN;
-            feature.properties.value = datajoin[countryname] || null;
+            const countryname = feature.properties.name;
+            feature.properties.data = datajoin[countryname] || null;
         });
 
-
-
+        // check the matching
+        console.log("Countries with data:", Object.keys(datajoin).length + " countries");
+        console.log("Countries without data:", geojson.features.filter(f => !f.properties.data).length + " countries");
 
         const platforms = [...new Set(Object.values(datajoin).map(d => d.topPlatform))];
         const platformValues = platforms.map((p, i) => i);
+
         const minValue = Math.min(...platformValues);
         const maxValue = Math.max(...platformValues);
         const midValue = minValue + (maxValue - minValue) / 2;
 
         var colorScale = d3.scaleDiverging([minValue, midValue, maxValue], d3.interpolatePiYG);
 
-
-
-        // Map platforms to numeric values
+        // mapping the platforms to numeric values
         const platformToValue = {};
         platforms.forEach((p, i) => platformToValue[p] = i);
 
-        // Initialize Leaflet map
         let map = L.map('map').setView([20, 0], 2);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 18,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
-
-
-        // Style function for countries
         function style(feature) {
-            //const data = feature.properties.data;
-            //const value = data ? platformToValue[data.topPlatform] : null;
-            //console.log(data);
+            const data = feature.properties.data;
+            const value = data ? platformToValue[data.topPlatform] : null;
             return {
-                fillColor: getColor(feature),
-                weight: 1,
-                opacity: 0.8,
+                fillColor: data ? colorScale(value) : '#e0e0e0',
+                weight: 0.5,
+                opacity: 1,
                 color: "white",
-                fillOpacity: 0.8,
+                fillOpacity: data ? 0.8 : 0.5,
             };
         }
 
-
-
-        L.geoJSON(data, {
-            style: function (feature) {
-                return {color: feature.properties.color};
-            }
-        })
-
-
-
-
-        // Create info control box
+        // making the info box
         var info = L.control();
         info.onAdd = function (map) {
             this._div = L.DomUtil.create('div', 'info');
@@ -119,22 +96,23 @@ d3.json("https://raw.githubusercontent.com/datasets/geo-countries/master/data/co
             return this._div;
         };
         info.update = function (props) {
-            //const data = props ? props.data : null;
-            this._div.innerHTML = '<h2>Music Streaming by Country</h2>' + (props ?
-                '<span>' + props.name + '</span><br />'
+            const data = props ? props.data : null;
+            this._div.innerHTML = '<h4>Music Streaming by Country</h4>' + (props ?
+                '<b>' + props.name + '</b><br />' +
+                (data ?
+                    '<strong>Top Platform:</strong> ' + data.topPlatform + '<br />' +
+                    '<strong>Top 3 Genres:</strong><br />' +
+                    '<ol style="margin: 5px 0; padding-left: 20px;">' +
+                    data.top3Genres.map(g => '<li>' + g + '</li>').join('') +
+                    '</ol>'
+                    : '<em>No data available</em>')
                 : 'Hover over a country');
         };
         info.addTo(map);
 
 
-
-
-
-
         function highlightFeature(e) {
             var layer = e.target;
-            //console.log(e.target.feature.properties.name);
-            //console.log(datajoin["Japan"])
             layer.setStyle({
                 weight: 5,
                 color: '#7496fa',
@@ -153,8 +131,6 @@ d3.json("https://raw.githubusercontent.com/datasets/geo-countries/master/data/co
             map.fitBounds(e.target.getBounds());
         }
 
-        //function
-
         function onEachFeature(feature, layer) {
             layer.on({
                 mouseover: highlightFeature,
@@ -168,36 +144,19 @@ d3.json("https://raw.githubusercontent.com/datasets/geo-countries/master/data/co
             onEachFeature: onEachFeature
         }).addTo(map);
 
-
         var legend = L.control({position: 'bottomright'});
         legend.onAdd = function (map) {
-            var div = L.DomUtil.create('div', 'info legend'),
-                platform_names=["Spotify", "YouTube", "Amazon Music", "Apple Music", "Deezer", "Tidal","Other"];
+            var div = L.DomUtil.create('div', 'info legend');
 
-            div.innerHTML = '<h4>Top Platforms</h4>';
-
-
-
-            for (var i = 0; i < platform_names.length; i++) {
+            div.innerHTML = '<h4>Top Platform</h4>';
+            platforms.forEach(platform => {
                 div.innerHTML +=
-                    '<i style="background:' + getColor(platform_names[i]) + '"></i> ' + platform_names[i] + '<br>'
-            }
-
-
+                    '<i style="background:' + colorScale(platformToValue[platform]) + '"></i> ' +
+                    platform + '<br>';
+            });
 
             return div;
         };
         legend.addTo(map);
-
-
-        //console.log("United States of America");
-
-
-
-
-
-
-
-
     });
 });
